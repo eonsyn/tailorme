e447619795ac1fccd823f3c128e696f05d20548a
aryansingh-0 <aryan03aryansingh@gmail.com>
2025-09-15 19:37:42 +0530

    update tehe backgened

--- backened/src/app.js
+++ backened/src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
 const cookieParser = require('cookie-parser')
-const rateLimit = require('express-rate-limit')
+const rateLimit = require("express-rate-limit");
 
 const routes = require('./routes')
 const errorHandler = require('./middlewares/errorHandler')
