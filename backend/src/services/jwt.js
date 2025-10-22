c68563d1c4bc4a9c75dd48859efc9ed98dd7d154
aryansingh-0 <aryan03aryansingh@gmail.com>
2025-10-22 14:30:33 +0530

    update

--- backend/src/services/jwt.js
+++ backend/src/services/jwt.js
@@ -48,7 +48,7 @@
     res.cookie('accessToken', accessToken, {
       httpOnly: true,
       secure: env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: env.NODE_ENV === 'production'? 'none':'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000,  
     })
   }
