9d3b6f15ee45fceffa155118531b418f10a58d82
aryansingh-0 <aryan03aryansingh@gmail.com>
2025-10-23 19:12:21 +0530

    update the api and work good for incognito

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,19 +1,32 @@
-// this is my next.config.mjs file can i write here 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-    images: {
+  images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
         pathname: '/**',
-      },{
+      },
+      {
         protocol: 'https',
         hostname: 'gptresume.vercel.app',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'tailorme-kappa.vercel.app',
+        pathname: '/**',
+      },
     ],
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://tailorme-kappa.vercel.app/:path*', // proxy backend
+      },
+    ];
   },
 };
 
